370df397f9645aa04da3b774da51d8b0616c855b
Irfan <alamandirfan@gmail.com>
2025-07-21 14:03:13 +0545

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = (props) => {
  let navigate = useNavigate();
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("${process.env.REACT_APP_API_URL ||http://localhost:5000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,7 +6,7 @@ const Signup = (props) => {
  let navigate = useNavigate();
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("${process.env.REACT_APP_API_URL ||http://localhost:5000/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
